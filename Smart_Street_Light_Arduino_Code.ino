f6fb19decb89196f90c56c0d8a85db44336acb5e
saimanikrishna <123304904+saimanikrishna@users.noreply.github.com>
2024-01-21 21:21:30 +0530

    Add files via upload

---
+++ Smart_Street_Light_Arduino_Code.ino
@@ -0,0 +1,58 @@
+int IR1 = 2;
+int IR2 = 3;
+int LED1 = 5;
+int LED2 = 6;
+int LDR = A3;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+  pinMode(LDR, INPUT);
+  
+  
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+}
+
+void loop() {
+
+  int LDRValue = analogRead(LDR);
+  Serial.print("sensor = ");
+  Serial.print(LDRValue);
+  delay (500);
+
+  if (LDRValue > 100)
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    Serial.println("It's Bright Outside; Lights status: OFF");
+  }
+  if ((LDRValue < 100) && (digitalRead(IR1) == HIGH)) 
+    {
+      digitalWrite(LED1, HIGH);
+      Serial.println("It's Dark Outside and No vehicle movement detected; LED1 Lights status: OFF");
+    }
+  
+  if ((LDRValue < 100) && (digitalRead(IR2) == HIGH)) 
+    {
+      digitalWrite(LED2, HIGH);
+      Serial.println("It's Dark Outside and No vehicle movement detected; LED2 Lights status: OFF");
+    }
+
+if ((LDRValue < 100) && (digitalRead(IR1) == LOW)) 
+    {
+      digitalWrite(LED1, HIGH);
+      Serial.println("It's Dark Outside and vehicle movement detected; LED1 Lights status: ON");
+    }
+  
+  if ((LDRValue < 100) && (digitalRead(IR2) == LOW)) 
+    {
+      digitalWrite(LED2, HIGH);
+      Serial.println("It's Dark Outside and vehicle movement detected; LED2 Lights status: ON");
+    }  
+}
